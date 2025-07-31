188f2d6fc2a6662f4b3fd72fd52f78d643b57e94
IzemC <prototype271295@protonmail.com>
2025-07-31 23:40:40 +0400

    dev setup

---
+++ backend/config/constants.js
@@ -0,0 +1,3 @@
+export const JWT_SECRET = process.env.JWT_SECRET || "secret";
+export const JWT_EXPIRES_IN = process.env.JWT_EXPIRES_IN || "24h";
+export const PORT = process.env.PORT || 5000;
---
+++ backend/config/db.js
@@ -0,0 +1,11 @@
+let users = [];
+let posts = [];
+let postReactions = [];
+let comments = [];
+
+export default {
+  users,
+  posts,
+  postReactions,
+  comments,
+};
